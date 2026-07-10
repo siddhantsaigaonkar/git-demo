85346640f81a2b6d7622443c149db4ad32930953
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-07-10 19:31:47 +0530

    chanages made

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Question from './components/Question';
 export default function App() {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>main Page</h1>
       <h2>features added</h2>
       <Question></Question>
       <h1>git end</h1>
